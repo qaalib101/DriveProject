bb9f6e271b1d52e715215feb9c1ec7b172ffb151
qaalib101 <si8822fb@go.minneapolis.edu>
2017-11-28 23:39:38 -0600

    vgg

--- src/main/java/com/company/Drive.java
+++ src/main/java/com/company/Drive.java
@@ -37,10 +37,7 @@ public class Drive {
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
-
-package com.google.api.services.samples.drive.cmdline;
+ * the License. */
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
